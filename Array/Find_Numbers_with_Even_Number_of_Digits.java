58a58eb3c573c5989458bb3e841c6f17bcdd8b9e
brucenguyen1852 <brucenguyen1852@gmail.com>
2024-06-27 09:31:45 +0700

    day 1

---
+++ Array/Find_Numbers_with_Even_Number_of_Digits.java
@@ -0,0 +1,60 @@
+package Array;
+
+public class Find_Numbers_with_Even_Number_of_Digits {
+    // Given an array nums of integers, return how many of them contain an even
+    // number of digits.
+
+    // Example 1:
+    // Input: nums = [12, 345, 2, 6, 7896]
+    // Output: 2
+    // Explanation:
+    // 12 contains 2 digits (even number of digits).
+    // 345 contains 3 digits (odd number of digits).
+    // 2 contains 1 digit (odd number of digits).
+    // 6 contains 1 digit (odd number of digits).
+    // 7896 contains 4 digits (even number of digits).
+    // Therefore only 12 and 7896 contain an even number of digits.
+
+    // Example 2:
+    // Input: nums = [555, 901, 482, 1771]
+    // Output: 1
+    // Explanation:
+    // Only 1771 contains an even number of digits.
+
+    public static void main(String[] args) {
+        int[] nums1 = { 12, 345, 2, 6, 7896 };
+        int[] nums2 = { 555, 901, 482, 1771 };
+
+        System.out.println("Input: nums1");
+        System.out.println("Expected Output: 2");
+        System.out.println("Actual Output: " + findNumbers(nums1)); // Output: 2
+
+        System.out.println("Input: nums2");
+        System.out.println("Expected Output: 1");
+        System.out.println("Actual Output: " + findNumbers(nums2)); // Output: 1
+    }
+
+    // T?m s? có s? lı?ng ch? s? là s? ch?n
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int digitCount = countDigits(nums[i]);
+            System.out.println("Number: " + nums[i] + ", Digit Count: " + digitCount);
+            if (digitCount % 2 == 0) {
+                count++;
+                System.out.println("Even Digit Count found. Current Even Count: " + count);
+            }
+        }
+        return count;
+    }
+
+    // ğ?m s? ph?n t? c?a 1 s?
+    public static int countDigits(int num) {
+        int count = 0;
+        while (num != 0) {
+            num /= 10;
+            count++;
+        }
+        return count;
+    }
+}
---
+++ Array/Max_Consecutive_Ones.java
@@ -0,0 +1,43 @@
+package Array;
+
+public class Max_Consecutive_Ones {
+    // Given a binary array nums, return the maximum number of consecutive 1's in
+    // the array.
+
+    // Example 1:
+
+    // Input: nums = [1,1,0,1,1,1]
+    // Output: 3
+    // Explanation: The first two digits or the last three digits are consecutive
+    // 1s. The maximum number of consecutive 1s is 3.
+    // Example 2:
+
+    // Input: nums = [1,0,1,1,0,1]
+    // Output: 2
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 0, 1, 1, 1 };
+        int maxCount = 0;
+        int currentCount = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                currentCount++;
+                // System.out.println("nums[" + i + "] = 1, currentCount = " + currentCount);
+            } else {
+                // System.out.println("nums[" + i + "] = 0, currentCount = " + currentCount);
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                    // System.out.println("New maxCount = " + maxCount);
+                }
+                currentCount = 0;
+            }
+        }
+
+        // Ki?m tra l?n cu?i ð? xem xét chu?i 1 liên ti?p cu?i cùng
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            // System.out.println("Final maxCount after loop = " + maxCount);
+        }
+        // System.out.println("Maximum number of consecutive 1s: " + maxCount);
+    }
+}
